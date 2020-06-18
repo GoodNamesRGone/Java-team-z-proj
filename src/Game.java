986b5cfec77cd86500369bcc4a213067d8fc7d8d
Mr. Bianchi <MisterBianchi@gmail.com>
2020-06-18 18:16:41 -0400

    Updated classes -JAB

--- src/Game.java
+++ src/Game.java
@@ -14,7 +14,7 @@ public class Game {
   public Game() {
 
     grid = new Grid(5, 10);// can edit
-   // grid.setMovableBackground(imgName, xOffset, yOffset, xScale, yScale);//offset can be 0,0. Background tbd
+    //grid.setBackground(imgName);
     userRow = 3;
     msElapsed = 0;
     timesGet = 0;
@@ -24,7 +24,11 @@ public class Game {
   }
   
   public void play() {
+    
+    //title screen
 
+    //start the game
+    grid.setMovableBackground(laserPic, 0, 0, 0.1, 0.1);//offset can be 0,0. Background tbd
     while (!isGameOver()) {
       grid.pause(100);
       handleKeyPress();
